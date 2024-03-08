adfbd87b6bf91605286003b43b50efe4a586055a
Sndx27 <138636041+Sndx27@users.noreply.github.com>
2024-03-08 07:43:52 +0000

    initial commit

---
+++ pertemuanPertama/cohort1.java
@@ -0,0 +1,7 @@
+package pertemuanPertama;
+
+public class cohort1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!, Hi Hu Tao (Sandy), Programming is so much fun!");
+    }
+}
